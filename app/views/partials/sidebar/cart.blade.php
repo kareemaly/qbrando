c66958cd0f679653f2aafdf23e02def7e79c6def
Kareem Mohamed <kareem3d.a@gmail.com>
2013-10-25 13:02:29 +0200

    Adding cart

---
+++ app/views/partials/sidebar/cart.blade.php
@@ -0,0 +1,4 @@
+<div class="main-primary-title" onclick="window.location.href='/shopping-cart.html'">
+    <span class="glyphicon glyphicon-shopping-cart"></span>Shopping cart: <b>0 items</b>
+</div><br />
+
