3a8dc3432eb004bd10c69e533f208064b3507119
Kareem Mohamed <kareem3d.a@gmail.com>
2013-10-26 14:36:09 +0200

    Changing class to extra bright

--- app/views/partials/sidebar/cart.blade.php
+++ app/views/partials/sidebar/cart.blade.php
@@ -1,4 +1,4 @@
-<div class="main-primary-title" onclick="window.location.href='/shopping-cart.html'">
+<div class="main-extra-bright-title" onclick="window.location.href='/shopping-cart.html'">
     <span class="glyphicon glyphicon-shopping-cart"></span>Shopping cart: <b ng-bind="cart.total()" ng-show="cart.isReady()"></b> <b>items</b>
 </div><br />
 
