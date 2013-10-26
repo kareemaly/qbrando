ae3aa90c4042cf0b0e2e891eb581001e5c406e07
Kareem Mohamed <kareem3d.a@gmail.com>
2013-10-27 00:10:46 +0200

    Adding google analytics code

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -10,18 +10,18 @@
 
     <script src="{{ URL::asset('app/lib/respond.min.js') }}"></script>
 
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-29205808-4', 'qbrando.com');
+        ga('send', 'pageview');
+
+    </script>
 </head>
 <body ng-controller="MainController">
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-29205808-4', 'qbrando.com');
-    ga('send', 'pageview');
-
-</script>
 
 <div class="large-container">
     <div class="container">
