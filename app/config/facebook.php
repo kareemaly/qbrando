6b1dd833bc923e7367f4b395c48ba4829a77ebd4
Kareem Mohamed <kareem3d.a@gmail.com>
2013-11-10 00:07:03 +0200

    Posting to facebook test 1

---
+++ app/config/facebook.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+
+    'config' => array(
+        'appId' => '577875652285919',
+        'secret' => '84727ebc3a01c100cc3e6f90190958ea',
+        'fileUpload' => false
+    ),
+
+    'access_token' => 'CAAINkyj6ld8BAL9fHxdj2zSI32CwuSQZChZAAOnfbVB7KbHDctcizRZCW2bCjZBGZAX4BLZCkzgwlNKMZAxxhfAypgsqa7d0fZAw9iaMYVhm4zsp6nGAZBO9ChJPIVmkE96WIuW1kD8LXovFHLlkICtxRGEwovygRlYxocV5E5QaGpqQy10m5qQZBT',
+
+
+    'page_id' => '420391658064864'
+
+);
