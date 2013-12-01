2da010d14e114d7274e90f82421bf7c691121010
Kareem Mohamed <kareem3d.a@gmail.com>
2013-12-01 21:14:53 +0200

    Removing special from small screens

--- app/views/partials/sidebar/specials.blade.php
+++ app/views/partials/sidebar/specials.blade.php
@@ -1,6 +1,6 @@
-<div class="main-title"><span class="glyphicon glyphicon-tag"></span>Special</div>
+<div class="main-title special-title"><span class="glyphicon glyphicon-tag"></span>Special</div>
 
-<div class="box">
+<div class="box special-box">
 
     @include('partials.product', array('product' => $special, 'showOfferPrice' => true))
 
