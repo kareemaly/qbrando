5c2684b9d8ab7c8386b581bbe6df8d7c8c869b1e
Kareem Mohamed <kareem3d.a@gmail.com>
2014-01-19 14:56:13 +0200

    Adding spaces

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -17,7 +17,7 @@
         <div class="cart-info">
             <a href="{{ URL::route('shopping-cart') }}">
                 <span>Shopping Cart: </span>
-            </a><strong ng-bind="cart.total()" ng-show="cart.isReady()"></strong> <strong>items</strong>
+            </a><strong ng-bind="cart.total()" ng-show="cart.isReady()"></strong> <strong> items</strong>
         </div>
     </div>
 </div>
